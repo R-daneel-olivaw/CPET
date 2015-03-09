90876510bdd0ec2095008a977e774b800755265d
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-09 16:08:07 -0400

    adding support for child processes for #4

--- module1/probes/Pobe.py
+++ module1/probes/Pobe.py
@@ -11,8 +11,8 @@ import csv
 
 class ProcessProbe:
     
-    PROCNAME = "vlc.exe"
-    procId = 0;
+    PROCNAME = None
+    procId = [];
     
     def __init__(self, processName, stepDelay=0.5):
         self.PROCNAME = processName
@@ -38,18 +38,42 @@ class ProcessProbe:
     def getProbeTargetName(self):
         return self.PROCNAME   
 
+
+    def getChildProcesses(self, proc_id):
+        
+        child_id = []
+        for p in proc_id:
+            try:
+                c_process = psutil.Process(p)
+                childs = c_process.children(recursive=True)
+                for chp in childs:
+                    child_id.append(chp.pid)
+            except:
+                print('process ', p, 'lost')
+                continue
+        
+        return child_id
+    
+    
     def startProbe(self):
-        procId = self.getPidForProcessName(self.PROCNAME)
-        print(procId)
+        self.procId.append(self.getPidForProcessName(self.PROCNAME))
+        
+        self.procId.append(self.getChildProcesses(self.procId))
+        
+        print(self.procId)
                 
                 # print(proc)
                 # print(proc.cpu_times())
-        if(procId != 0): 
+        for p in self.procId: 
+            
+            if(p==0):
+                self.procId.remove(0)
+                continue
             
             fileCsv = open('D:/Lectures/Winter2015/CS854/project/PickelTest/' + self.PROCNAME + '.csv', 'a')
             writer = csv.writer(fileCsv, delimiter=',', quoting=csv.QUOTE_NONE, lineterminator='\n')
             
-            proc = psutil.Process(procId)
+            proc = psutil.Process(p)
             
             try:
                 for i in range(0, 150):
@@ -59,22 +83,23 @@ class ProcessProbe:
                     diskIo = proc.get_io_counters()
                     netc = len(proc.connections())
                                         
-                    print(procId, 'cpu = ', cpu)
-                    print(procId, 'memory = ', mem)
-                    print(procId, 'disk_read_count = ', diskIo[0])
-                    print(procId, 'disk_write_count = ', diskIo[1])
-                    print(procId, 'disk_read_bytes = ', diskIo[2])
-                    print(procId, 'disk_write_bytes = ', diskIo[3])
-                    print(procId, 'network counters = ', netc)
+                    print(p, 'cpu = ', cpu)
+                    print(p, 'memory = ', mem)
+                    print(p, 'disk_read_count = ', diskIo[0])
+                    print(p, 'disk_write_count = ', diskIo[1])
+                    print(p, 'disk_read_bytes = ', diskIo[2])
+                    print(p, 'disk_write_bytes = ', diskIo[3])
+                    print(p, 'network counters = ', netc)
                     print()
                     
-                    rec = ProcRecord(cpu, mem, diskIo[0], diskIo[1], diskIo[2], diskIo[3],netc)
+                    rec = ProcRecord(cpu, mem, diskIo[0], diskIo[1], diskIo[2], diskIo[3], netc)
                     
                     sleep(self.stepDelay)
                     
                     self.addToCSV(writer, rec)
             except:
                 print("process lost..")
+                self.procId.remove(p)
             finally:
                 if fileCsv:
                     fileCsv.close()
