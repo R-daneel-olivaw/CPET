59314b61bdf0fafcee20d019519e8b863014edd7
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-09 23:42:53 -0400

    removed hard code process id

--- module1/probes/Pobe.py
+++ module1/probes/Pobe.py
@@ -112,8 +112,8 @@ class ProcessProbe:
     
     def startProbe(self):
         
-        # parent_id = self.getPidForProcessName(self.PROCNAME)
-        parent_id = 7832
+        parent_id = self.getPidForProcessName(self.PROCNAME)
+        #parent_id = 7832
         self.p_map[parent_id] = [parent_id]
         
         # self.procId.append(self.getPidForProcessName(self.PROCNAME))
