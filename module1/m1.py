74742849f3c4ecd3c7be8f992d4026e37e522aa5
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-31 16:39:34 -0400

    module 1 updaed for #13

--- module1/m1.py
+++ module1/m1.py
@@ -3,15 +3,34 @@ Created on Mar 6, 2015
 
 @author: Akshat
 '''
-process_names = ['chrome.exe', 'thunderbird.exe', 'eclipse.exe']
+process_names = None
 
-output_path = 'C:/Users/Akshat/git/CPET/output/m1/'
+output_path = None
 
+
+def parse_config(ini_path):
+    
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read(ini_path)
+    
+    m1_config = config['m1']
+    
+    global process_names
+    process_names = m1_config['process_name_list'].split(',')
+    global output_path
+    output_path = m1_config['output_path']
+    
 def exem1():
+    
+    parse_config('../cpet_config.ini')
+    print('process_names = ',process_names)
+    print('output_path = ',output_path)
     
     ProbeThreadController(process_names, output_path).start_probes()
     
 
 if __name__ == '__main__':
     from module1.probe_thread_controller import ProbeThreadController
+    import configparser
     exem1()
