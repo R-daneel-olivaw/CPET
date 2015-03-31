7b544e2c4da6f86ab12b3b886d4744761bc9e0f1
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-31 16:49:47 -0400

    Module 2 changed for #13

--- module2/m2.py
+++ module2/m2.py
@@ -3,19 +3,40 @@ Created on Mar 6, 2015
 
 @author: Akshat
 '''
-output_directory_path = 'C:/Users/Akshat/git/CPET/output/m2'
+output_file_path = None
+input_log_file_path_list = None
+
+
+def parse_config(ini_path):
+    
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read(ini_path)
+    
+    m2_config = config['m2']
+    
+    global output_file_path
+    output_file_path = m2_config['output_path']
+    
+    global input_log_file_path_list
+    input_log_file_path_list = m2_config['input_log_file_path_list'].split(',')
 
 def exem2():
-    probeAgg = PrbLogAgg(output_directory_path, csvFilePath=['C:/Users/Akshat/git/CPET/output/m1/chrome.exe.csv', 'C:/Users/Akshat/git/CPET/output/m1/thunderbird.exe.csv','C:/Users/Akshat/git/CPET/output/m1/eclipse.exe.csv'])
+    
+    parse_config('../cpet_config.ini')
+    
+    print('output_file_path = ',output_file_path)
+    print('input_log_file_path_list = ',input_log_file_path_list)
+    
+    probeAgg = PrbLogAgg(output_file_path, csvFilePath= input_log_file_path_list)
     probeAgg.loadDataFrame()
 
     a = Analyst(probeAgg)
     # print(df)
 
     report_dict = a.calculate_percentiles()
-    print(report_dict)
     
-    m2o.generate_op(report_dict, output_directory_path+'/Output2.xml')
+    m2o.generate_op(report_dict, output_file_path)
     
     # print(percentile_df.describe())
     # print('Percentile = /n', percentile_df)
@@ -24,4 +45,5 @@ if __name__ == '__main__':
     from module2.ProbeLogAggregator import PrbLogAgg
     from module2.toolkit.dfAnalyst1 import Analyst
     import module2.toolkit.m2_output as m2o
+    import configparser
     exem2()
